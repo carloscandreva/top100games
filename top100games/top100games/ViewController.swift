7d56d4632783a9f040413c89c5cb7e3c8aa3ef87
Carlos Alberto Mota Candreva <carlosmc@ciandt.com>
2018-07-20 19:41:13 -0300

    Initial Project

---
+++ top100games/top100games/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  top100games
+//
+//  Created by Carlos Alberto Mota Candreva on 20/07/2018.
+//  Copyright © 2018 Carlos Alberto Mota Candreva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
